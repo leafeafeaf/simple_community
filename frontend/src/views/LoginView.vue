5f716c6de45e1b0c0f4aac6fcb9bf921b739aeef
dispear <87793524+dispear@users.noreply.github.com>
2024-02-02 18:07:55 +0900

    로그인 ver 1

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,58 @@
-<template lang="">
-  <div>로그인화면입니다.</div>
+<template>
+  <form action="get">
+    <div class="id-div">
+      <div class="id-div-div">
+        <span class="id">아이디</span> <input type="text" name="" id="" />
+      </div>
+    </div>
+    <div class="pw-div">
+      <div class="pw-div-div">
+        <span class="pw">비밀번호</span> <input type="password" name="" id="" />
+      </div>
+    </div>
+    <div class="btn-div">
+      <input class="btn" type="button" value="로그인" />
+    </div>
+  </form>
 </template>
 <script>
 export default {};
 </script>
-<style lang=""></style>
+<style>
+.id-div {
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+}
+.id-div-div {
+  width: 250px;
+  display: flex;
+  justify-content: space-between;
+}
+.id {
+  font-weight: bold;
+}
+
+.pw-div {
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+}
+.pw-div-div {
+  width: 250px;
+  display: flex;
+  justify-content: space-between;
+}
+.pw {
+  font-weight: bold;
+}
+
+.btn-div {
+  margin: 10px;
+}
+.btn {
+  width: 250px;
+  padding: 5px;
+  font-weight: bold;
+}
+</style>
