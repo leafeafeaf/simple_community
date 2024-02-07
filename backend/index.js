b007bf220edc7eb145b634bcd1f2f9c4587f0bbd
김성민 <rlatjdals369@naver.com>
2024-02-07 14:52:46 +0900

    DB 1차 실패

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');//익스프레스 서버
 const path = require('path');   // 윈도우 주소 쓰기 위해서
 const dotenv = require('dotenv'); //비밀키들 모아놓는곳
 const morgan = require('morgan'); //로그 기록용
+const { sequelize } = require('./models');
 
 //비밀키 불러옴 (process.env로 불러올 수 있음)
 dotenv.config();
@@ -9,6 +10,15 @@ dotenv.config();
 //서버 포트 설정
 const app = express();
 app.set('port', process.env.PORT || 3000);
+
+//DB ORM 연결 (sequelize)
+sequelize.sync({force:false})
+    .then(()=>{
+        console.log('데이터베이스 연결 성공');
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 
 //미들웨어 설정
 app.use('/files',express.static(path.join(__dirname,'public'))); //public폴더 정적파일화
@@ -18,15 +28,24 @@ app.use(
     express.urlencoded({extended:false}),           //url 주소 (params, query)이런친구들 해석
 )
 
-
 //라우터
 const routeRouter = require('./routes/route');
 app.use('/',routeRouter);
 
 //404라우터 (주소 일치하는게 하나도 없을때)
 app.use((req,res,next)=>{
-    res.status(404).send('Not Found');
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+
+    next(error);
 });
+
+app.use((err,req,res,next)=>{
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+})
 
 app.listen(app.get('port'),()=>{
     console.log(app.get('port'), '번 포트에서 대기 중');
