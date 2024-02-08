8afc0a76eeb314111f1779a068dc7a8791c776ae
김성민 <rlatjdals369@naver.com>
2024-02-08 15:51:37 +0900

    메인화면 ver2

--- frontend/src/views/ContentView.vue
+++ frontend/src/views/ContentView.vue
@@ -1,5 +1,15 @@
 <template lang="">
   <div>상세 게시글 화면 입니다.</div>
+  <div>
+    <div>제목</div>
+    <div>게시글 내용</div>
+    <div>추천</div>
+  </div>
+  <div>
+    <div>댓글 리스트</div>
+    <div>댓글 번호</div>
+    <div>댓글 쓰기</div>
+  </div>
 </template>
 <script>
 export default {};
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -6,7 +6,31 @@
       <div>조회수순</div>
       <div>추천순</div>
     </div>
-    <div class="content-list-div">게시글 리스트</div>
+    <div class="content-list-div">
+      게시글 리스트
+      <div
+        class="content-list-for"
+        v-for="content in contentList"
+        :key="content"
+        @click="this.$router.push('content')"
+      >
+        <div class="content-list-detail">
+          <div class="content-recom-num">{{ content.recom_num }}</div>
+          <div>|</div>
+          <div>
+            <div class="detail-title">
+              <div class="content-title">{{ content.title }}</div>
+              <div class="content-comment-num">[{{ content.comment_num }}]</div>
+            </div>
+            <div class="detail-inform">
+              <div class="content-date">2초전 /</div>
+              <div class="content-writer">{{ content.writer }}</div>
+            </div>
+          </div>
+        </div>
+        <div class="content-view-num">{{ content.view_num }}</div>
+      </div>
+    </div>
     <div class="down-button-div">
       <button class="write-button">
         <router-link to="/write">글쓰기</router-link>
@@ -36,7 +60,53 @@
   </div>
 </template>
 <script>
-export default {};
+export default {
+  setup() {}, //페이지 생성시 실행되는 코드
+
+  data() {
+    //변수 선언
+    return {
+      contentList: [
+        {
+          content_id: 1,
+          title: "롤하자",
+          writer: "야민철",
+          date: new Date(),
+          view_num: 5,
+          comment_num: 6,
+          recom_num: 7,
+        },
+        {
+          content_id: 2,
+          title: "롤하자2",
+          writer: "야민철",
+          date: new Date(),
+          view_num: 5,
+          comment_num: 6,
+          recom_num: 7,
+        },
+        {
+          content_id: 3,
+          title: "롤하자3",
+          writer: "야민철",
+          date: new Date(),
+          view_num: 5,
+          comment_num: 6,
+          recom_num: 7,
+        },
+      ],
+    };
+  },
+  methods() {
+    //함수 설정하는 곳
+    return {
+      //게시글 리스트 불러오기 api
+      getContentList() {
+        return 10;
+      },
+    };
+  },
+};
 </script>
 <style>
 .sort-div {
@@ -56,6 +126,54 @@ export default {};
 .content-list-div {
   background-color: aqua;
   height: 500px;
+}
+
+.content-list-for {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: blanchedalmond;
+  margin: 5px;
+  padding: 0 16px;
+}
+.content-list-detail {
+  display: flex;
+  align-items: center;
+}
+.content-list-detail > div {
+  margin: 8px;
+}
+.detail-title {
+  display: flex;
+  align-items: center;
+}
+.detail-inform {
+  display: flex;
+  align-items: center;
+}
+.content-writer {
+  font-size: 12px;
+}
+
+.content-title {
+  color: blue;
+  margin-right: 8px;
+}
+
+.content-title:hover {
+  text-decoration: underline;
+}
+.content-date {
+  color: rgb(158, 157, 157);
+  font-size: 12px;
+  margin-right: 8px;
+}
+.content-recom-num {
+}
+.content-comment-num {
+  color: blue;
+}
+.content-view-num {
 }
 
 .down-button-div {
