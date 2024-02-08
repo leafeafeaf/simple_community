a2b784b120b52e35e314c9a5cbeada1a49728f0a
dispear <87793524+dispear@users.noreply.github.com>
2024-02-08 17:13:53 +0900

    상세 게시글 ver1

--- frontend/src/views/ContentView.vue
+++ frontend/src/views/ContentView.vue
@@ -1,17 +1,174 @@
 <template lang="">
   <div>상세 게시글 화면 입니다.</div>
   <div>
-    <div>제목</div>
-    <div>게시글 내용</div>
-    <div>추천</div>
+    <div>
+      <hr />
+      <div class="title-div">
+        <div>{{ contentDetail.title }}</div>
+        <div>{{ contentDetail.date }}</div>
+      </div>
+      <hr />
+      <div class="writer-div">
+        <div>{{ contentDetail.writer }}</div>
+        <div class="viewnum-div">
+          <div>조회 수 : {{ contentDetail.view_num }}</div>
+          <div>추천 수 : {{ contentDetail.recom_num }}</div>
+          <div>댓글 : {{ contentDetail.comment_num }}</div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <textarea class="content" name="" id="" rows="30">게시글</textarea>
+    </div>
+    <div class="recom-div">
+      <div class="recom">{{ contentDetail.recom_num }}</div>
+      <input class="recom-btn" type="button" value="좋아요" />
+    </div>
   </div>
+  <hr />
   <div>
-    <div>댓글 리스트</div>
+    <div>
+      <div v-for="comment in commentList" :key="comment">
+        <div class="comment-writer-div">
+          <div class="comment-writer">{{ comment.writer }}</div>
+          <div class="comment-date">{{ comment.date }}</div>
+        </div>
+        <div class="comment-content-div">{{ comment.content }}</div>
+        <hr />
+      </div>
+    </div>
     <div>댓글 번호</div>
-    <div>댓글 쓰기</div>
+    <div>
+      <div class="comment-write">댓글 쓰기</div>
+      <div class="comment-post">
+        <textarea
+          class="comment-textarea"
+          name=""
+          id=""
+          cols="1"
+          rows="3"
+        ></textarea>
+        <input class="comment-btn" type="button" value="등록" />
+      </div>
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      contentDetail: {
+        content_id: 1,
+        title: "롤 하자",
+        writer: "야민철",
+        content: "롤좀하자 야성민",
+        file: "1.jpg",
+        date: new Date(),
+        view_num: 5,
+        comment_num: 6,
+        recom_num: 7,
+      },
+      commentList: [
+        {
+          comment_id: 1,
+          content: "싫엉",
+          date: new Date(),
+          writer: "야정찬",
+          content_id: 4,
+        },
+        {
+          comment_id: 2,
+          content: "싫엉22222",
+          date: new Date(),
+          writer: "야민호",
+          content_id: 4,
+        },
+        {
+          comment_id: 3,
+          content: "싫엉33333333",
+          date: new Date(),
+          writer: "야성민",
+          content_id: 4,
+        },
+      ],
+    };
+  },
+};
 </script>
-<style lang=""></style>
+<style scoped>
+.title-div {
+  display: flex;
+  justify-content: space-between;
+  margin: 8px;
+}
+.writer-div {
+  display: flex;
+  justify-content: space-between;
+  margin-left: 8px;
+  margin-bottom: 8px;
+}
+.viewnum-div {
+  display: flex;
+  justify-content: space-between;
+}
+.viewnum-div > div {
+  margin-right: 8px;
+}
+.content {
+  width: 100%;
+  resize: none;
+  margin-bottom: 10px;
+  border-right: 0px solid black;
+  border-left: 0px solid black;
+}
+.recom-div {
+  margin: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.recom {
+  color: blue;
+  border: 1px solid black;
+  padding: 10px;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.recom-btn {
+  margin: 16px 0px;
+  width: 100px;
+}
+
+.comment-writer-div {
+  display: flex;
+  justify-content: space-between;
+  margin: 8px;
+}
+.comment-write {
+  display: flex;
+  justify-content: left;
+  margin: 8px;
+}
+.comment-post {
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+}
+.comment-content-div {
+  text-align: left;
+  margin: 8px;
+}
+
+.comment-textarea {
+  width: 90%;
+  resize: none;
+  margin-right: 8px;
+  padding: 5px;
+}
+.comment-btn {
+}
+</style>
