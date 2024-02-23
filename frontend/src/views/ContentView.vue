f262990279eb3d7bbeac3ba35dfacb18e90307b9
dispear <87793524+dispear@users.noreply.github.com>
2024-02-23 17:01:12 +0900

    댓글 기능

--- frontend/src/views/ContentView.vue
+++ frontend/src/views/ContentView.vue
@@ -30,12 +30,21 @@
   <hr />
   <div>
     <div>
-      <div v-for="comment in commentList" :key="comment">
+      <div v-for="(comment, index) in commentList" :key="comment">
         <div class="comment-writer-div">
           <div class="comment-writer">{{ comment.writer }}</div>
           <div class="comment-date">{{ comment.date }}</div>
         </div>
-        <div class="comment-content-div">{{ comment.content }}</div>
+        <div class="comment-content-div">
+          <div class="comment-content">{{ comment.content }}</div>
+          <div
+            class="comment-delete"
+            v-if="this.equlCommentWriter(comment.writer)"
+            @click="this.deleteComment(comment.comment_id, index)"
+          >
+            삭제
+          </div>
+        </div>
         <hr />
       </div>
     </div>
@@ -49,8 +58,14 @@
           id=""
           cols="1"
           rows="3"
+          v-model="comment_content"
         ></textarea>
-        <input class="comment-btn" type="button" value="등록" />
+        <input
+          class="comment-btn"
+          type="button"
+          value="등록"
+          @click="postComment"
+        />
       </div>
     </div>
   </div>
@@ -66,37 +81,18 @@ export default {
   },
   created() {
     this.getContentDetail(this.content_id);
+    this.getCommentList();
   },
   data() {
     return {
       contentDetail: {},
       is_like: false,
-      commentList: [
-        {
-          comment_id: 1,
-          content: "싫엉",
-          date: new Date(),
-          writer: "야정찬",
-          content_id: 4,
-        },
-        {
-          comment_id: 2,
-          content: "싫엉22222",
-          date: new Date(),
-          writer: "야민호",
-          content_id: 4,
-        },
-        {
-          comment_id: 3,
-          content: "싫엉33333333",
-          date: new Date(),
-          writer: "야성민",
-          content_id: 4,
-        },
-      ],
+      commentList: [],
+      comment_content: "",
     };
   },
   methods: {
+    // 게시글 불러오기 api 호출
     getContentDetail(content_id) {
       this.axios
         .get("/content/" + content_id, {
@@ -114,8 +110,12 @@ export default {
           console.error(err);
         });
     },
-    //삭제 api 호출
+    // 게시글 삭제 api 호출
     deleteContent() {
+      if (this.store.state.gid == this.contentDetail.writer) {
+        alert("작성한 사용자만 삭제할 수 있습니다.");
+        return;
+      }
       this.axios
         .delete("/content/" + this.content_id)
         .then((res) => {
@@ -133,6 +133,10 @@ export default {
 
     //수정 페이지로 이동
     pushModify() {
+      if (this.store.state.gid == this.contentDetail.writer) {
+        alert("작성한 사용자만 수정할 수 있습니다.");
+        return;
+      }
       this.$router.push({
         //이동인데 뒤로가기 됨
         name: "modify",
@@ -140,6 +144,68 @@ export default {
           contentDetail: JSON.stringify(this.contentDetail),
         },
       });
+    },
+    // 댓글 리스트 불러오기 api 호출
+    getCommentList() {
+      this.axios
+        .get("/comment/list/" + this.content_id)
+        .then((res) => {
+          this.commentList = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    // 댓글 작성 api 호출
+    postComment() {
+      if (!this.comment_content) {
+        alert("댓글을 입력해주세요");
+        return;
+      }
+      if (!this.store.state.gid) {
+        alert("로그인 해주세요");
+        this.$router.push({
+          name: "login",
+        });
+        return;
+      }
+      this.axios
+        .post("/comment", {
+          content: this.comment_content,
+          content_id: this.content_id,
+          writer: this.store.state.gid,
+        })
+        .then((res) => {
+          this.commentList.push(res.data);
+          this.comment_content = "";
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    // 댓글 삭제 api 호출
+    deleteComment(comment_id, index) {
+      if (this.store.state.gid == this.commentList[index].writer) {
+        alert("작성한 사용자만 삭제할 수 있습니다.");
+        return;
+      }
+      this.axios
+        .delete("/comment/" + comment_id)
+        .then((res) => {
+          this.commentList.splice(index, 1);
+          alert("삭제 되었습니다.");
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    equlCommentWriter(writer) {
+      if (this.store.state.gid == writer) {
+        return true;
+      } else {
+        return false;
+      }
     },
   },
   computed: {
@@ -218,8 +284,13 @@ export default {
   justify-content: center;
 }
 .comment-content-div {
-  text-align: left;
   margin: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+.comment-content {
+  text-align: left;
 }
 
 .comment-textarea {
