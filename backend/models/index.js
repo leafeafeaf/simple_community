07775adef87a0a30c03884da683bc99122c38d2b
김성민 <rlatjdals369@naver.com>
2024-02-29 16:39:10 +0900

    config변경

--- backend/models/index.js
+++ backend/models/index.js
@@ -10,7 +10,15 @@ const Recommendation = require('./recommendation');
 //.env 파일에 production이 아니면 개발중으로 인식
 const env = process.env.NODE_ENV || 'development';
 //const config = require('../config/config')[env];
-const config = process.env.DB;
+//const config = process.env.DB;
+const config={
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT 
+}
 const db = {};
 //config/config의 developement 부분을 가져옴
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
